fc4b5944b6e89d33ff2350126d61471126148615
Venus Ang <venusang24@gmail.com>
2018-02-07 19:54:23 -0800

    Configure HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebPackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   entry: __dirname + '/app/index.js',
   module: {
     loaders: [
       {
-        test: ?\.js$/, excluse: /node_modules/,
+        test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
       }
     ]
@@ -11,5 +19,6 @@ module.exports = {
   output: {
     filename: 'transformed.js',
     path: __dirname + '/build'
-  }
+  },
+  plugins: [HTMLWebPackPluginConfig]
 };
