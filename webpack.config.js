648b27527dbb38789401f26d81cc7d58c0998d85
Venus Ang <venusang24@gmail.com>
2018-02-07 17:42:04 -0800

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  module: {
+    loaders: [
+      {
+        test: ?\.js$/, excluse: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  }
+};
