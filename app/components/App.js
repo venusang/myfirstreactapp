4b12b14e9eb227dfa86377fb65c182c4e609b93f
Venus Ang <venusang24@gmail.com>
2018-02-07 21:15:14 -0800

    Writing my first App class, creating my first build, adding new dev script

---
+++ app/components/App.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function App() {
+  return <p>This is App.js and it's boring alskhdlakshdkshdksjhdlkshlkjsh</p>;
+}
+
+module.exports = App;
---
+++ app/components/Welcome.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+module.exports = Welcome;
